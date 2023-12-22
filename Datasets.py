94109c5da09352e0572722ee168d206e25f36bdc
Mathieu Chalvid <mathieu.chalvidal@gmail.com>
2023-12-22 18:08:01 -0500

    Cleaning MNIST and PDE

--- Datasets.py
+++ Datasets.py
@@ -2,6 +2,48 @@ from torch.utils.data import DataLoader, Dataset
 from torchvision import datasets, transforms
 import numpy as np
 import torch
+import torch.nn.functional as F
+
+
+
+def shuffle(x):
+    #ps = np.random.randint(2)+1
+    ps = int(np.random.choice([1,2,4,7]))
+    #ps = 1
+    idx = torch.randperm(int(784/(ps*ps)))
+    #if torch.rand(1).item()>0.5:
+    #    x = x.transpose(-2,-1)
+    u = F.unfold(x, kernel_size=ps, stride=ps, padding=0)[:,:,idx]
+    f = F.fold(u, x.shape[-2:], kernel_size=ps, stride=ps, padding=0)
+    return f
+
+def project(x):
+    with torch.no_grad():
+        A = torch.empty(1,784,784,device='cuda').normal_(mean=0,std=np.sqrt(1/784)).repeat(x.shape[0],1,1)
+    return torch.bmm(A,x.unsqueeze(-1).cuda()).squeeze(-1)
+
+def shuffle_label(y):
+    idx = np.random.permutation(10)
+    y_ = torch.tensor([idx[a] for a in y])
+    #y = torch.nn.functional.one_hot(y_, num_classes=10)
+    return y_
+
+def generate_sample(b,t):
+    bs = b.shape[0]
+    x = shuffle(b.unsqueeze(1)).reshape(bs,-1)
+    #x = project(b)
+    y = shuffle_label(t)
+    #print(x.shape,x.reshape(bs,-1).shape,batch[1].shape)
+    return x, y #torch.cat([x.reshape(bs,-1),y.unsqueeze(-1)/10],dim=-1)# x.reshape(bs,-1), y #torch.cat([x.reshape(bs,-1),y],dim=-1), y
+
+def generate_sample_noperm(batch,bs):
+    #idx = torch.randperm(784)
+    b_ = batch[0].reshape(bs,-1)#[:,idx]
+    return b_, batch[1]
+
+
+
+
 
 
 class Dataset_ADR(Dataset):
@@ -9,18 +51,32 @@ class Dataset_ADR(Dataset):
         data = np.load(source_file)
         # sensors, y_train, coeffs = Data['sensors'], Data['y_train'], Data['coeffs']
         self.y_train = data['y_train'].reshape(500, 100, 100, 100)
+        self.data_lenght = len(self.y_train)
         # self.sensors = Data['sensors']
         # self.coeffs  = Data['coeffs']
 
-    def torch_wrapper(self, array):
-        tensor = torch.from_numpy(array).to('cuda').float()
+    def torch_wrapper(self, array, device):
+        tensor = torch.from_numpy(array).to(device).float()
         return tensor
+    
+    def sample_batch(self,data,n,device):
+        idx = torch.randperm(len(data))[:n]
+        t_target = np.random.randint(30,100)
+        x, y = data[idx,:,0],data[idx,:,t_target]
+        return self.torch_wrapper(x,device), self.torch_wrapper(y,device)
 
-    def __getitem__(self, index):
-        y = self.torch_wrapper(self.y_train[index])
-        # u = self.torch_wrapper(self.sensors[index])
-        # c = self.coeffs[index]
-        return y  # ,c#, u#, c
+    def meta_batch(self,N,n,device):
+        b_, t_ = [],[]
+        for k in range(N):
+            c = np.random.choice(range(self.data_lenght))
+            b, target = self.sample_batch(self.y_train[c],n,device)
+            b_.append(b)
+            t_.append(target) 
+        return torch.stack(b_,dim=0),torch.stack(t_,dim=0)   
+
+    
+    def __getitem__(self, index, device):
+        return self.y_train[index] 
 
     def __len__(self):
         return len(self.y_train)
@@ -40,18 +96,42 @@ class Dataset_Burgers(Dataset):
 
     def __len__(self):
         return len(self.y_train)
+    
+    
+class Dataset_MNIST_Like(object):
+    """
+    Class for MNIST_like data
+    """
+    def __init__(self,dataset):
+        self.data_lenght = len(dataset.data)
+        self.data = dataset.data
+        self.target = dataset.targets
+        
+    def sample_batch(self,n,device):
+        idx = torch.randperm(self.data_lenght)[:n]
+        d, t = generate_sample(self.data[idx].float(),self.target[idx])
+        return d.reshape(n,-1).to(device), t.to(device)
+    
+    def meta_batch(self,N,n,device):
+        b_, t_ = [],[]
+        for k in range(N):
+            b, target = self.sample_batch(n,device)
+            b_.append(b)
+            t_.append(target) 
+        return torch.stack(b_,dim=0),torch.stack(t_,dim=0)
+    
+    def __len__(self):
+        return self.data_lenght
 
 
 
-def Meta_dataset(path, type, batch_size=100):
-    if type == 'ADR':
-        return Dataset_ADR(path +'/ADR_l=0.2.npz')
-    elif type == 'Burgers':
-        return Dataset_Burgers(path +'/Burgers.npz')
-    elif type == 'MNIST_like':
-        dataset = datasets.MNIST('/media/data_cifs_lrs/projects/prj_control/mathieu/unsupervised/MNIST/', train=True,
-                                 transform=transforms.ToTensor(), download=False)
-        train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-        return train_loader
+def Meta_dataset(path, type_, batch_size=100):
+    if type_ == 'ADR':
+        return Dataset_ADR(path +'ADR_l=0.2.npz')
+    elif type_ == 'Burgers':
+        return Dataset_Burgers(path +'Burgers.npz')
+    elif type_ == 'MNIST':
+        dataset = datasets.MNIST(path, train=True, transform=transforms.ToTensor(), download=False)
+    return Dataset_MNIST_Like(dataset)
 
 
--- utils.py
+++ utils.py
@@ -5,14 +5,30 @@ from sklearn import gaussian_process as gp
 import scipy.fftpack
 from scipy.interpolate import interp2d
 from torch.utils.data import DataLoader, Dataset
+import torch
+import yaml
+
+def prepare_target(target, n_func_pred, device, xp_type='ADR'):
+    
+    if xp_type=='ADR':
+        t_ = target.clone()
+        t_[:,:n_func_pred] = 0.
+        t = target
+        
+    elif xp_type=='MNIST':
+        t = torch.nn.functional.one_hot(target, num_classes=10)
+        t_ = t.clone()
+        t_[:,:n_func_pred] = 0.
+        
+    return t.to(device).float(), t_.to(device).float()
 
 
 def sample_batch_ADR(s, n, num):
     perm = torch.randperm(n)[:num]
     idx_u = 0
     idx_s = -1
-    x = s[perm, :, idx_u]
-    y = s[perm, :, idx_s]
+    x = s[perm,:, idx_u]
+    y = s[perm,:, idx_s]
     return x, y
 
 def sample_batch_Burgers(s, n, num):
@@ -25,19 +41,38 @@ def sample_batch_Burgers(s, n, num):
     # s_bis = s[idx_sbis,perm].float()
     return u_, s_  # , s_bis
 
+def sample_batch_MNIST(s, n, num):
+    perm = torch.randperm(n)[:num]
+    idx_u = 0
+    idx_s = -1
+    x = s[perm, :, idx_u]
+    y = s[perm, :, idx_s]
+    return x, y
+
+def generate_batch(meta_bs):
+    b_, t_ = [],[]
+    for i, b in enumerate(train_loader):
+        ba,target = generate_sample(b,bs)
+        b_.append(ba)
+        t_.append(target)
+        if i==meta_bs-1:
+            return torch.stack(b_,dim=0),torch.stack(t_,dim=0)
+
+
 def get_sampler(XP_type):
     if XP_type == 'ADR':
         return sample_batch_ADR
     elif XP_type == 'Burgers':
         return sample_batch_Burgers
+    elif XP_type == 'MNIST':
+        return sample_batch_Burgers
+        
 
 def get_loss(XP_type):
     if XP_type in ['ADR','Burgers','MNIST']:
         def loss_function(y, y_hat):
             return torch.pow(y - y_hat, 2).mean()
         return loss_function
-
-
 
 
 def loss_function(y, y_hat):
@@ -76,8 +111,7 @@ def generate_sample(batch, bs):
     # x = project(batch[0].reshape(bs,-1))
     y = shuffle_label(batch[1])
     # print(x.shape,x.reshape(bs,-1).shape,batch[1].shape)
-    return x.reshape(bs,
-                     -1), y  # torch.cat([x.reshape(bs,-1),y.unsqueeze(-1)/10],dim=-1)# x.reshape(bs,-1), y #torch.cat([x.reshape(bs,-1),y],dim=-1), y
+    return x.reshape(bs,-1), y  # torch.cat([x.reshape(bs,-1),y.unsqueeze(-1)/10],dim=-1)# x.reshape(bs,-1), y #torch.cat([x.reshape(bs,-1),y],dim=-1), y
 
 
 def generate_sample_noperm(batch, bs):
@@ -99,5 +133,16 @@ def generate_batch(meta_bs):
         t_.append(target)
         if i == meta_bs - 1:
             return torch.stack(b_, dim=0), torch.stack(t_, dim=0)
+        
+def get_config(XP_type):
+    if XP_type=='ADR':
+        config_path = './config_ADR.yaml'
+    elif XP_type=='Burgers':
+        config_path = './config_Burgers.yaml'
+    elif XP_type=='MNIST':
+        config_path = './config_MNIST.yaml'
+    with open(config_path) as f:
+        cfg = yaml.load(f, Loader=yaml.FullLoader)
+    return cfg
 
 
